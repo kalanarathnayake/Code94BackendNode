7ea62950ee994ab5c3c8f2f8d57370cde65023b8
kalanarathnayake <kalanagayanga8@gmail.com>
2023-05-04 00:08:31 +0530

    first commit

---
+++ controller/Product.controller.js
@@ -0,0 +1,77 @@
+const Product = require("../model/product.model");
+
+// add Product function
+const addProduct = async (req, res) => {
+    const { sku, quantity, productName, imgUrl, productDescription, isFavourite } = req.body;
+    const product = new Product({
+        sku,
+        quantity,
+        productName,
+        imgUrl,
+        productDescription,
+        isFavourite,
+    });
+
+    await product.save()
+        .then(() => res.json('product created!' + product))
+        .catch(err => res.status(400).json('Error (create): ' + err));
+}
+
+// update Product function
+const updateProduct = async (req, res) => {
+    Product.findByIdAndUpdate(req.params.id)
+        .then((existingProduct) => {
+            existingProduct.sku = req.body.sku;
+            existingProduct.quantity = req.body.quantity;
+            existingProduct.productName = req.body.productName;
+            existingProduct.imgUrl = req.body.imgUrl;
+            existingProduct.productDescription = req.body.productDescription;
+            existingProduct.isFavourite = req.body.isFavourite;
+
+            existingProduct.save()
+                .then((updatedProduct) => res.json("Product Details Updated : " + updatedProduct))
+                .catch((error) => res.status(400).json("Error:" + error));
+        })
+        .catch((error) => res.status(400).json("Error (update): " + error));
+}
+
+// delete Product function
+const deleteProduct = async (req, res) => {
+    console.log(req.params.id);
+    Product.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Product deleted'))
+        .catch(err => res.status(400).json('Error (delete): ' + err));
+};
+
+// get  Product function by id
+const getProductById = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (product)
+            res.json(product);
+        else {
+            res.json("No product from this ID")
+        }
+    } catch (error) {
+        res.status(500).send("Server Error : " + error);
+    }
+};
+
+// get all Product function
+const getProduct = async (req, res) => {
+    try {
+        const product = await Product.find();
+        res.json(product);
+    } catch (error) {
+        res.status(500).send("Server Error (getAll): " + error);
+    }
+}
+
+//export 
+module.exports = {
+    addProduct,
+    getProductById,
+    deleteProduct,
+    getProduct,
+    updateProduct,
+};
---
+++ model/product.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    sku: { type: String, required: true},
+    quantity: { type: String, required: true },
+    productName: { type: String, required: true },
+    imgUrl: { type: String, required: true },
+    productDescription: { type: String, required: true },
+    isFavourite: { type: Boolean, required: true }
+}, {
+    timestamps: true,
+})
+
+module.exports = Product = mongoose.model("Products", productSchema);
