a17be1f476a30f44978c6c3d346b4086cbdc0e0b
rusiruavb <rusiru.bandara@arimaclanka.com>
2023-05-05 03:25:29 +0530

    fix controller issues

--- controller/Product.controller.js
+++ controller/Product.controller.js
@@ -1,79 +1,87 @@
-const Product = require("../model/product.model");
+const Product = require('../model/product.model');
 
 // add Product function
 const addProduct = async (req, res) => {
-    const { sku, quantity, productName, imgUrl, productDescription, isFavourite, price } = req.body;
-    const product = new Product({
-        sku,
-        quantity,
-        productName,
-        imgUrl,
-        productDescription,
-        isFavourite,
-        price
-    });
+  const {
+    sku,
+    quantity,
+    productName,
+    imgUrl,
+    productDescription,
+    isFavourite,
+    price,
+  } = req.body;
+  const product = new Product({
+    sku,
+    quantity,
+    productName,
+    imgUrl,
+    productDescription,
+    isFavourite,
+    price,
+  });
 
-    await product.save()
-        .then(() => res.json('product created!' + product))
-        .catch(err => res.status(400).json('Error (create): ' + err));
-}
+  await product
+    .save()
+    .then(() => res.json('product created!' + product))
+    .catch((err) => res.status(400).json('Error (create): ' + err));
+};
 
 // update Product function
 const updateProduct = async (req, res) => {
-    Product.findByIdAndUpdate(req.params.id)
-        .then((existingProduct) => {
-            existingProduct.sku = req.body.sku;
-            existingProduct.quantity = req.body.quantity;
-            existingProduct.productName = req.body.productName;
-            existingProduct.imgUrl = req.body.imgUrl;
-            existingProduct.productDescription = req.body.productDescription;
-            existingProduct.isFavourite = req.body.isFavourite;
-            existingProduct.price = req.body.price;
+  Product.findByIdAndUpdate(req.params.id)
+    .then((existingProduct) => {
+      existingProduct.sku = req.body.sku;
+      existingProduct.quantity = req.body.quantity;
+      existingProduct.productName = req.body.productName;
+      existingProduct.imgUrl = req.body.imgUrl;
+      existingProduct.productDescription = req.body.productDescription;
+      existingProduct.isFavourite = req.body.isFavourite;
+      existingProduct.price = req.body.price;
 
-            existingProduct.save()
-                .then((updatedProduct) => res.json("Product Details Updated : " + updatedProduct))
-                .catch((error) => res.status(400).json("Error:" + error));
-        })
-        .catch((error) => res.status(400).json("Error (update): " + error));
-}
+      existingProduct
+        .save()
+        .then((updatedProduct) => res.json({ updatedProduct }))
+        .catch((error) => res.status(400).json('Error:' + error));
+    })
+    .catch((error) => res.status(400).json('Error (update): ' + error));
+};
 
 // delete Product function
 const deleteProduct = async (req, res) => {
-    console.log(req.params.id);
-    Product.findByIdAndDelete(req.params.id)
-        .then(() => res.json('Product deleted'))
-        .catch(err => res.status(400).json('Error (delete): ' + err));
+  Product.findByIdAndDelete(req.params.id)
+    .then(() => res.json(`Product deleted - ${req.params.id}`))
+    .catch((err) => res.status(400).json('Error (delete): ' + err));
 };
 
 // get  Product function by id
 const getProductById = async (req, res) => {
-    try {
-        const product = await Product.findById(req.params.id);
-        if (product)
-            res.json(product);
-        else {
-            res.json("No product from this ID")
-        }
-    } catch (error) {
-        res.status(500).send("Server Error : " + error);
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) res.json(product);
+    else {
+      res.json('No product from this ID');
     }
+  } catch (error) {
+    res.status(500).send('Server Error : ' + error);
+  }
 };
 
 // get all Product function
 const getProduct = async (req, res) => {
-    try {
-        const product = await Product.find();
-        res.json(product);
-    } catch (error) {
-        res.status(500).send("Server Error (getAll): " + error);
-    }
-}
+  try {
+    const product = await Product.find();
+    res.json(product);
+  } catch (error) {
+    res.status(500).send('Server Error (getAll): ' + error);
+  }
+};
 
-//export 
+//export
 module.exports = {
-    addProduct,
-    getProductById,
-    deleteProduct,
-    getProduct,
-    updateProduct,
+  addProduct,
+  getProductById,
+  deleteProduct,
+  getProduct,
+  updateProduct,
 };
--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
-app.use(cors()); //cors origin
-app.use(express.json()); 
+app.use(cors({ origin: ['http://localhost:3000'] })); //cors origin
+app.use(express.json());
+app.use(express.urlencoded());
 const mongoose = require('mongoose');
-// mongoose.set('strictQuery', false); 
-const port = process.env.PORT || 5000
+// mongoose.set('strictQuery', false);
+const port = process.env.PORT || 8091;
 
 app.get('/', (req, res) => {
   res.send('ITP Backend API Running');
-})
+});
 
-connectMongoDB().then(()=>console.log("MongoDB connection established!!")).catch(err => console.log(err));
+connectMongoDB()
+  .then(() => console.log('MongoDB connection established!!'))
+  .catch((err) => console.log(err));
 
 async function connectMongoDB() {
-  await mongoose.connect('mongodb+srv://code94:code94_1234@cluster0.ioecl5j.mongodb.net/?retryWrites=true&w=majority');
+  await mongoose.connect(
+    'mongodb+srv://code94:code94_1234@cluster0.ioecl5j.mongodb.net/?retryWrites=true&w=majority'
+  );
 }
 
 app.use('/product', require('./route/product.route'));
@@ -22,4 +28,4 @@ app.use('/admin', require('./route/Admin.route'));
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
-})
+});
